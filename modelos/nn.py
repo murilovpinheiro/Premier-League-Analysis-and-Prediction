cd9d64de1ef22515b854936552316cb2b5ace6fe
Murilo Pinheiro <murilovpinheiro@gmail.com>
2023-06-28 16:25:58 -0300

    Fazendo funções, ajeitando gráficos e criando XGB para binário

--- modelos/nn.py
+++ modelos/nn.py
@@ -7,6 +7,7 @@ import torch.nn as nn
 import torch.optim as optim
 
 from sklearn.model_selection import train_test_split
+from sklearn.metrics import confusion_matrix
 
 import matplotlib.pyplot as plt
 
@@ -18,6 +19,7 @@ class Multiclass(nn.Module):
         
         # utilizando ELU como função de ativação
         self.act = nn.ELU()
+        self.act2 = nn.Tanh()
         self.outputact = nn.Softmax(dim = 1)
         
         self.hidden1 = nn.Linear(input_size, 128)
@@ -32,11 +34,11 @@ class Multiclass(nn.Module):
         
     def forward(self, x):
         x = self.hidden1(x)
-        x = self.act(x)
-        x = self.dropout(x)  # Aplicar Dropout após a primeira camada oculta
+        x = self.act2(x)
+        x = self.dropout(x)
 
         x = self.hidden2(x)
-        x = self.act(x)
+        x = self.act2(x)
         x = self.dropout(x)  
 
         x = self.hidden3(x)
@@ -55,8 +57,7 @@ class Multiclass(nn.Module):
         x = self.outputact(x)
         return x
 
-
-def trainValidation(train_x, train_y):
+def trainValidation(train_x, train_y, n_epochs):
     x_train = torch.tensor(train_x).float()
     y_train = torch.from_numpy(train_y).float()
 
@@ -72,11 +73,11 @@ def trainValidation(train_x, train_y):
     input_size = x_train.shape[1]
     output_size = y_train.shape[1]
 
-    n_epochs = 200 # Queria mais mas acho que 200 tá bom
+    # parametros eu defini como esses mesmo
 
-    momentums = [0.7, 0.8, 0.9]
-    learning_rates = [0.1, 0.01, 0.001]
-    weight_decays = [0.001, 0.0001, 0.00005] 
+    momentums = [0.7, 0.8]
+    learning_rates = [0.1, 0.01]
+    weight_decays = [0.0001, 0.0005] 
 
     best_acc = - np.nan
     models = []
@@ -100,7 +101,7 @@ def trainValidation(train_x, train_y):
                             parameters.append({"learning_rate": lr,
                                             "momentum": momentum, 
                                             "weight_decay": weight_decay})
-                                    
+                            print(lr, momentum,weight_decay)
                             batches_per_epoch = len(x_train) // batch_size
                             model = Multiclass(input_size, output_size, 0.2)
                                     
@@ -136,14 +137,16 @@ def trainValidation(train_x, train_y):
                                     
                                     model.eval()
                                     y_pred = model(x_valid)
-                                    print(y_pred.mean(axis = 0))
-                                    print(y_valid.mean(axis = 0))
+
+                                    #print(y_pred.mean(axis = 0))
+                                    #print(y_valid.mean(axis = 0))
+
                                     ce = loss_fn(y_pred, y_valid)
                                     acc = (torch.argmax(y_pred, 1) == torch.argmax(y_valid, 1)).float().mean()
 
                                     ce = float(ce)
                                     acc = float(acc)
-                                    print(f"Epoch {epoch} train: Cross-entropy={sum(epoch_loss)/batches_per_epoch}, Accuracy={sum(epoch_acc)/batches_per_epoch}")
+                                    #print(f"Epoch {epoch} train: Cross-entropy={sum(epoch_loss)/batches_per_epoch}, Accuracy={sum(epoch_acc)/batches_per_epoch}")
                                     
                                     current_CROSS_train.append(np.mean(epoch_loss))
                                     current_ACC_train.append(np.mean(epoch_acc))
@@ -154,7 +157,9 @@ def trainValidation(train_x, train_y):
                                     if acc > best_acc:
                                             best_acc = acc
                                             best_weights = copy.deepcopy(model.state_dict())
-                                    print(f"Epoch {epoch} validation: Cross-entropy={ce}, Accuracy={acc}")
+                                    #print(f"Epoch {epoch} validation: Cross-entropy={ce}, Accuracy={acc}")
+
+                                    print(f"Epoch {epoch} Finalizada.")
                                     
                                     CROSS_losses_train.append(current_CROSS_train)
                                     CROSS_losses_val.append(current_CROSS_val)
@@ -177,34 +182,34 @@ def trainValidation(train_x, train_y):
     print(parameters[indice_menor])
     parameters_ = parameters[indice_menor]
 
-    epochs = range(1, len(CROSS_losses_train) + 1)
+    epochs = range(1, len(best_CROSS_train) + 1)
 
     fig, ax = plt.subplots(figsize = (10, 8))
-    ax.plot(epochs, best_CROSS_train, 'b', label='Train Cross-Entropy')
-    ax.plot(epochs, best_CROSS_val, 'r', label='Validation Cross-Entropy')
-    plt.title('Cross-Entropy per Epoch')
-    plt.xlabel('Epoch')
-    plt.ylabel('Cross-Entropy Value')
+    ax.plot(epochs, best_CROSS_train, 'b', label='Valor da Função de Perda (Cross-Entropy) Treino')
+    ax.plot(epochs, best_CROSS_val, 'r', label='alor da Função de Perda (Cross-Entropy) Validação')
+    plt.title('Função de Perda ao longo das Épocas da Rede Neural')
+    plt.xlabel('Épocas')
+    plt.ylabel('Valor da Cross-Entropy')
     plt.legend()
     ax.grid(True)
-    fig.savefig("./CEValidation.png")
+    fig.savefig("./gráficos/NN/CEValidation.png")
 
-    epochs = range(1, len(ACC_losses_train) + 1)
+    epochs = range(1, len(best_ACC_train) + 1)
     fig, ax = plt.subplots(figsize = (10, 8))
-    ax.plot(epochs, best_ACC_train, 'b', label='Train Acc')
-    ax.plot(epochs, best_ACC_val, 'r', label='Validation Acc')
-    plt.title('Acc per Epoch')
-    plt.xlabel('Epoch')
-    plt.ylabel('Acc Value')
+    ax.plot(epochs, best_ACC_train, 'b', label='Acurácia do Treino')
+    ax.plot(epochs, best_ACC_val, 'r', label='Acurácia da Validação')
+    plt.title('Acurácia ao longo das Épocas da Rede Neural')
+    plt.xlabel('Épocas')
+    plt.ylabel('Valor da Acurácia')
     plt.legend()
     ax.grid(True)
-    fig.savefig("./ACCValidation.png")
+    fig.savefig("./gráficos/NN/ACCValidation.png")
 
     return parameters_
 
 
 
-def trainTest(x_train, y_train, x_test, y_test, parameters):
+def trainTest(x_train, y_train, x_test, y_test, parameters, n_epochs):
     x_train = torch.tensor(x_train).float()
     y_train = torch.from_numpy(y_train).float()
 
@@ -219,8 +224,6 @@ def trainTest(x_train, y_train, x_test, y_test, parameters):
 
     input_size = x_train.shape[1]
     output_size = y_train.shape[1]
-
-    n_epochs = 300
 
     momentum = parameters["momentum"]
     learning_rate = parameters["learning_rate"]
@@ -239,8 +242,6 @@ def trainTest(x_train, y_train, x_test, y_test, parameters):
 
     ACC_train = []
     ACC_test = []
-
-    n_epochs = 300
 
     loss_fn = nn.CrossEntropyLoss()
     optimizer = optim.SGD(model.parameters(), lr=learning_rate, weight_decay = weight_decay, momentum = momentum)
@@ -294,26 +295,27 @@ def trainTest(x_train, y_train, x_test, y_test, parameters):
 
     epochs = range(1, len(CROSS_train) + 1)
     fig, ax = plt.subplots(figsize = (10, 8))  
-    ax.plot(epochs, CROSS_train, 'b', label='Train Cross-Entropy')
-    ax.plot(epochs, CROSS_test, 'r', label='Test Cross-Entropy')
-    plt.title('Cross-Entropy per Epoch')
-    plt.xlabel('Epoch')
-    plt.ylabel('Cross-Entropy Value')
+    ax.plot(epochs, CROSS_train, 'b', label='Valor da Função de Perda (Cross-Entropy) Treino')
+    ax.plot(epochs, CROSS_test, 'r', label='Valor da Função de Perda (Cross-Entropy) Teste')
+    plt.title('Função de Perda ao longo das Épocas da Rede Neural')
+    plt.xlabel('Épocas')
+    plt.ylabel('Valor da Cross-Entropy')
     plt.legend()
     ax.grid(True)
-    fig.savefig("./CETest.png")
+    fig.savefig("./gráficos/NN/CETest.png")
 
 
     epochs = range(1, len(ACC_train) + 1)
     fig, ax = plt.subplots(figsize = (10, 8))
-    ax.plot(epochs, ACC_train, 'b', label='Train Accuracy')
-    ax.plot(epochs, ACC_test, 'r', label='Test Accuracy')
-    plt.title('Accuracy per Epoch')
-    plt.xlabel('Epoch')
-    plt.ylabel('Accuracy Value')
+    ax.plot(epochs, ACC_train, 'b', label='Acurácia do Treino')
+    ax.plot(epochs, ACC_test, 'r', label='Acurácia do Teste')
+    plt.title('Acurácia ao longo das Épocas da Rede Neural')
+    plt.xlabel('Épocas')
+    plt.ylabel('Valor da Acurácia')
     plt.legend()
     ax.grid(True)
-    fig.savefig("./ACCTest.png")
+    plt.show()
+    fig.savefig("./gráficos/NN/ACCTest.png")
 
 
     # Último teste
@@ -327,6 +329,45 @@ def trainTest(x_train, y_train, x_test, y_test, parameters):
     print("Erro(CROSS) do teste: ", test_loss)
     print("Acurácia do teste: ", acc.item())
 
+    # Converter as saídas do teste e os rótulos do teste em numpy arrays
+    test_outputs_np = test_outputs.detach().numpy()
+    y_test_np = y_test.detach().numpy()
 
-def testando():
-    print("Olá")
+    # Obter as classes previstas (índice com maior valor de probabilidade)
+    predicted_classes = np.argmax(test_outputs_np, axis=1)
+
+    # Obter as classes verdadeiras (índice com valor 1 em one-hot encoding)
+    true_classes = np.argmax(y_test_np, axis=1)
+
+    # Calcular a matriz de confusão
+    confusion = confusion_matrix(true_classes, predicted_classes)
+
+    # Normalizar a matriz de confusão
+    confusion_normalized = confusion.astype('float') / confusion.sum(axis=1)[:, np.newaxis]
+
+    # Definir os rótulos das classes
+    class_labels = ['A', 'D', 'H']
+
+    # Plotar a matriz de confusão
+    plt.imshow(confusion_normalized, interpolation='nearest', cmap=plt.cm.Blues)
+    plt.title('Matriz de Confusão Normalizada da Rede Neural')
+    plt.colorbar()
+    ticks = np.arange(len(class_labels))
+    plt.xticks(ticks, class_labels)
+    plt.yticks(ticks, class_labels)
+
+    # Adicionar os valores na matriz
+    thresh = confusion_normalized.max() / 2.
+    for i in range(confusion_normalized.shape[0]):
+        for j in range(confusion_normalized.shape[1]):
+            plt.text(j, i, format(confusion_normalized[i, j], '.2f'), horizontalalignment="center",
+                     color="white" if confusion_normalized[i, j] > thresh else "black")
+
+    plt.ylabel('Classe Verdadeira')
+    plt.xlabel('Classe Prevista')
+    plt.tight_layout()
+
+    # Salvar a figura
+    plt.savefig('./gráficos/NN/confusionMatrixTest.png')
+
+    return test_loss, acc 
